16f0b1943e4e18391146a98c286215e9d7d00e3f
iamalamgir <mdalamgir.pro@gmail.com>
2024-05-20 14:51:22 +0600

    Users image added and testimonials section completed.

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './Testimonials.css';
 import next_icon from '../../assets/next.png';
 import back_icon from '../../assets/prev.png';
-import user_1 from '../../assets/siam.jpg';
-import user_2 from '../../assets/siam.jpg';
-import user_3 from '../../assets/siam.jpg';
-import user_4 from '../../assets/siam.jpg';
+import user_1 from '../../assets/rana.jpg';
+import user_2 from '../../assets/nayeem.jpg';
+import user_3 from '../../assets/akhtur.jpg';
+import user_4 from '../../assets/nirjhor.jpg';
 
-const Testimonials = ({subTitle, Title}) => {
+const Testimonials = () => {
+    
+    const slider = useRef();
+    let tx = 0;
+
+    const slideForward = ()=>{
+        if(tx > -50){
+            tx -= 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+    const slideBackward = ()=>{
+        if(tx < 0){
+            tx += 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+
+
     return (
         <div className='testimonials'>
-            <img src={next_icon} alt="" className='next-btn' />
-            <img src={back_icon} alt="" className='back-btn' />
+            <img src={next_icon} alt="" className='next-btn' onClick={slideForward} />
+            <img src={back_icon} alt="" className='back-btn' onClick={slideBackward} />
             <div className="slider">
-                <ul>
+                <ul ref={slider}>
                     <li>
                         <div className="slide">
                             <div className="user-info">
@@ -36,7 +54,7 @@ const Testimonials = ({subTitle, Title}) => {
                             <div className="user-info">
                                 <img src={user_2} alt="" />
                                 <div>
-                                    <h3>Sohel Rana</h3>
+                                    <h3>Abul Hasnat Nayeem</h3>
                                     <span>AHUSity, Bangladesh</span>
                                 </div>
                             </div>
@@ -53,7 +71,7 @@ const Testimonials = ({subTitle, Title}) => {
                             <div className="user-info">
                                 <img src={user_3} alt="" />
                                 <div>
-                                    <h3>Sohel Rana</h3>
+                                    <h3>Akhtur Uz Zaman</h3>
                                     <span>AHUSity, Bangladesh</span>
                                 </div>
                             </div>
@@ -70,7 +88,7 @@ const Testimonials = ({subTitle, Title}) => {
                             <div className="user-info">
                                 <img src={user_4} alt="" />
                                 <div>
-                                    <h3>Sohel Rana</h3>
+                                    <h3>Sazzad Hossain Nirjhor</h3>
                                     <span>AHUSity, Bangladesh</span>
                                 </div>
                             </div>

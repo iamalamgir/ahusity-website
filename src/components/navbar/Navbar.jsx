59b16438447298265aab070e4acbe630178d563f
iamalamgir <mdalamgir.pro@gmail.com>
2024-05-26 00:20:34 +0600

    Add scroll and responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import './Navbar.css';
 import cap from '../../assets/cap.png';
+import menu_icon from '../../assets/menu-icon.png';
+import { Link } from "react-scroll";
 
 const Navbar = () => {
 
@@ -12,22 +14,28 @@ const Navbar = () => {
         })
     },[]);
 
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const toggleMenu = ()=>{
+        mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
+
   return (
     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
         <div className="logo">
             <img src={cap} atl="" className="cap" />
             <h2>AHUSity</h2>
         </div>
-        <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li><button className="btn">Contact us</button></li>
+        <ul className={mobileMenu?'':'hide-mobile-menu'}>
+           <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+           <li><Link to='programs' smooth={true} offset={0} duration={500}>Program</Link></li>
+           <li><Link to='about' smooth={true} offset={0} duration={500}>About us</Link></li>
+           <li><Link to='campus' smooth={true} offset={0} duration={500}>Campus</Link></li>
+           <li><Link to='testimonials' smooth={true} offset={0} duration={500}>Testimonials</Link></li>
+           <li><Link to='contact' smooth={true} offset={0} duration={500} className="btn">Contact us</Link></li>
         </ul>
+        <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
